2bc05052034d1cd7f8d1f95b907406940237df0e
Sonu Kumar <sonuflag159@gmail.com>
2021-12-28 10:11:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,39 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Router>
+        <NavBar />
+        {/* <LoadingBar
+        color='#f11946'
+        progress={this.state.progress}
+      /> */}
+        <Routes>
+          <Route  path="/" element={<News key="general" pageSize={6} country="in" category="general" />}>
+            {/* // <News key="general" pageSize={6} country="in" category="general" /> */}
+          </Route>
+          <Route  path="/business" element = {<News key="business" pageSize={6} country="in" category="business" />}>
+            {/* <News key="business" pageSize={6} country="in" category="business" /> */}
+          </Route>
+          <Route  path="/entertainment" element = {<News key="entertainment" pageSize={6} country="in" category="entertainment" />}>
+            {/* <News key="entertainment" pageSize={6} country="in" category="entertainment" /> */}
+          </Route>
+          <Route  path="/general" element = {<News key="general" pageSize={6} country="in" category="general" />}>
+            {/* <News key="general" pageSize={6} country="in" category="general" /> */}
+          </Route>
+          <Route  path="/health" element = {<News key="health" pageSize={6} country="in" category="health" />}>
+            {/* <News key="health" pageSize={6} country="in" category="health" /> */}
+          </Route>
+          <Route  path="/science" element = {<News key="science" pageSize={6} country="in" category="science" />}>
+            {/* <News key="science" pageSize={6} country="in" category="science" /> */}
+          </Route>
+          <Route  path="/sports" element = {<News key="sports" pageSize={6} country="in" category="sports" />}>
+            {/* <News key="sports" pageSize={6} country="in" category="sports" /> */}
+          </Route>
+          <Route  path="/technology" element = {<News key="technology" pageSize={6} country="in" category="technology" />}>
+            {/* <News key="technology" pageSize={6} country="in" category="technology" /> */}
+          </Route>
+        </Routes>
+      </Router>
       </div>
     )
   }
